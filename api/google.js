ce1bb62ebaf9a3a8fb83546f8768c82541b125a7
Oskar Anderberg <73995193+Norven94@users.noreply.github.com>
2025-03-02 16:42:31 +0100

    google test

---
+++ api/google.js
@@ -0,0 +1,32 @@
+const calendar = require('@googleapis/calendar')
+
+
+
+async function googleTest() {
+    const gauth = new calendar.auth.GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/calendar']
+      });
+    const authClient = await gauth.getClient();
+    
+    const client = await calendar.calendar({
+        version: 'v3',
+        auth: authClient
+    });
+    
+    const events = await client.events.list({
+        calendarId: 'primary',
+        timeMin: new Date().toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+    }).then(res => res.data.items)
+    
+    events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+    })
+}
+
+
+
+module.exports = googleTest;
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cron = require('node-cron')
 const scraper = require('./scraper')
+const googleTest = require('./google')
 const app = express();
 
 
@@ -25,12 +26,24 @@ app.get("/run-cron", async (req, res) => {
       }
 })
 
+app.get("/google-test", async (req, res) => {
+  try {
+    await googleTest()
+    return res.status(200).json({
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }  
+})
+
 const runCron = () => {
   scraper()
 }
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
-cron.schedule('*/2 * * * *', runCron)
+// cron.schedule('*/2 * * * *', runCron)
 
 module.exports = app;
